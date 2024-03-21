78fec301f6457b506b58a10b3fafa6bc856fe042
shemTuri254 <bsclmr157320@spu.ac.ke>
2024-03-21 18:54:11 +0300

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,15 @@
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+import { Lusitana } from 'next/font/google';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
